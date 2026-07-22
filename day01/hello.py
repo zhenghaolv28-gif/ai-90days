b496d6f1ac389ce994d5f0666a6a81cab6db207f
zhenghaolv28-gif <zhenghaolv28@gmail.com>
2026-07-22 17:22:34 +0800

    Update hello.py

--- day01/hello.py
+++ day01/hello.py
@@ -1,4 +1,14 @@
-我的名称：Deku
-目标岗位：与AI相关的应用生成师
-每天的学习时间2-5h
-90天预计学习时间：360-450
+nickname = "Deku"
+target_role = "AI应用开发相关岗位"
+
+days = 90
+min_hours_per_day =2
+mix_hours_perr_day = 5
+
+min_total_hours = days * min_hours_per_day
+max_total_hours = days * max_hours_per_day
+
+print(f"我的名字:{nickname}")
+print(f"目标岗位:{target_role}")
+print(f"每天学习时间:{min_hours_per_day}-{max_hours_per_day}")
+print(f"90天预计学习时间:{min_total_hours}-{max_total_hours}")
